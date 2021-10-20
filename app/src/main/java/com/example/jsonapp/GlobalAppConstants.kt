c018e72633d594d280937a26dd7ab00677411343
Hassan Amer <hsn999hsn@gmail.com>
2021-10-20 10:55:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jsonapp/GlobalAppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.jsonapp
+
+class GlobalAppConstants {
+    companion object{
+        const val title = "global app constants"
+        const val errorMessage = "\"Enter number first\""
+    }
+}
